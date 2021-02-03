fe1bb53fdc71dfcc92ba0671887c92b391ad917e
Massimo Biancalani <massimo.biancalani@gmail.com>
2021-02-03 16:08:49 +0100

    day 9

--- day_09_transfiguration/day09.cpp
+++ day_09_transfiguration/day09.cpp
@@ -1,4 +1,29 @@
 //
 // Created by Massimo Biancalani on 02/02/2021.
 //
-
+#include <string>
+#include <map>
+using namespace std;
+int transfigure (string A, string B)
+{
+    if (A.size() != B.size()) {
+        return -1;
+    }
+    map<char,int> mapForA, mapForB;
+    for(int i = 0; i < A.size(); ++i) {
+        mapForA[A[i]]++;
+        mapForB[B[i]]++;
+    }
+    if (!equal(mapForA.begin(), mapForA.end(), mapForB.begin())) {
+        return -1;
+    }
+    int result = 0;
+    for(int i = A.size() -1, j = B.size() -1 ; i >= 0; --i) {
+        if (A[i] == B[j]) {
+            --j;
+        } else {
+            result++;
+        }
+    }
+    return result;
+}
